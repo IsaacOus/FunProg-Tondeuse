d98057eeb57325f1579fa45bf347bf2496f007d8
IsaacOus <iouslimane@myges.fr>
2023-02-03 08:28:33 +0100

    3rd test + refactored Direction

--- src/main/scala/progfun/tondeuse/TondeusePlatform.scala
+++ src/main/scala/progfun/tondeuse/TondeusePlatform.scala
@@ -5,25 +5,25 @@ import progfun.position.Position
 
 object TondeusePlatform {
 
-  val InitialCoords: (Int, Int) = (1, 1);
+  val InitialCoords: (Int, Int) = (1, 1)
 
   def initializeTondeuse(
       coords: (Int, Int),
-      direction: Direction
+      direction: Char
   ): Either[TondeuseError, TondeuseState] = {
     for {
-      direction <- toDirection(direction.toString)
+      direction <- toDirection(direction)
     } yield TondeuseState(Position(coords), direction)
   }
 
   private def toDirection(
-      direction: String
+      direction: Char
   ): Either[TondeuseError, Direction] = {
     direction match {
-      case "N" => Right(NORTH)
-      case "S" => Right(SOUTH)
-      case "E" => Right(EST)
-      case "W" => Right(WEST)
+      case 'N' => Right(NORTH)
+      case 'S' => Right(SOUTH)
+      case 'E' => Right(EST)
+      case 'W' => Right(WEST)
       case _   => Left(TondeuseError("Invalid direction"))
     }
   }
--- src/test/scala/example/TondeuseSpec.scala
+++ src/test/scala/example/TondeuseSpec.scala
@@ -2,32 +2,43 @@ package example
 
 import org.scalatest.funsuite.AnyFunSuite
 import progfun.direction.{NORTH, SOUTH}
-import progfun.tondeuse.TondeusePlatform._
 import progfun.position.Position
+import progfun.tondeuse.TondeusePlatform._
 import progfun.tondeuse.TondeuseState
 
 class TondeuseSpec extends AnyFunSuite {
 
   test("initialize with a valid starting point and a north direction") {
-    val InitialCoords = (1, 1);
-    val InitialDirection = NORTH;
+    val InitialCoords = (1, 1)
+    val InitialDirection = 'N'
 
-    val tondeuseState = initializeTondeuse(InitialCoords, InitialDirection);
+    val tondeuseState = initializeTondeuse(InitialCoords, InitialDirection)
     tondeuseState match {
-      case Right(TondeuseState(Position(InitialCoords), InitialDirection)) =>
+      case Right(TondeuseState(Position(InitialCoords), NORTH)) =>
       case _ =>
         fail("Tondeuse should be initialized with valid coords and direction")
     }
   }
+
   test("initialize with a valid starting point and a south direction") {
-    val InitialCoords = (0, 0);
-    val InitialDirection = SOUTH;
+    val InitialCoords = (0, 0)
+    val InitialDirection = 'S'
 
-    val tondeuseState = initializeTondeuse(InitialCoords, InitialDirection);
+    val tondeuseState = initializeTondeuse(InitialCoords, InitialDirection)
     tondeuseState match {
-      case Right(TondeuseState(Position(InitialCoords), InitialDirection)) =>
+      case Right(TondeuseState(Position(InitialCoords), SOUTH)) =>
       case _ =>
         fail("Tondeuse should be initialized with valid coords and direction")
     }
   }
+
+  test("initialize with an invalid direction") {
+    val tondeuseState = initializeTondeuse((0, 0), 'Z')
+    tondeuseState match {
+      case Left(_) =>
+      case _ =>
+        fail("Tondeuse should not be initialized with an invalid direction")
+    }
+  }
+
 }
