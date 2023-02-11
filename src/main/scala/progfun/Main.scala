e17c00cfe6ad90095f2940d57fbbc7237f3b3313
IsaacOus <iouslimane@myges.fr>
2023-02-11 08:10:09 +0100

    Added file parser

--- src/main/scala/progfun/Main.scala
+++ src/main/scala/progfun/Main.scala
@@ -1,5 +1,17 @@
-package fr.esgi.al.funprog
+package progfun
+
+import better.files.File
+import progfun.parsing.Parser
 
 object Main extends App {
-  println("Ici le programme principal")
+  val file: File = File("src/main/resources/sample_input.txt")
+
+  val parser = new Parser(file)
+  val mapSize: (Int, Int) = parser.extractMapSize()
+  val tondeuses = parser.extractTondeusePosition()
+  val commands = parser.extractCommands()
+
+  println(mapSize)
+  println(tondeuses)
+  println(commands)
 }
---
+++ src/main/scala/progfun/parsing/Parser.scala
@@ -0,0 +1,37 @@
+package progfun.parsing
+
+import better.files._
+
+class Parser(file: File) {
+
+  private val input = extractLines(this.file)
+
+  private def extractLines(file: File) = {
+    val lines = file.contentAsString
+    lines
+  }
+
+  def extractMapSize(): (Int, Int) = {
+    val lines = input.split("\n")
+    val sizeLine = lines.find(line => line.count(_.isDigit) >= 2)
+    sizeLine match {
+      case Some(line) => {
+        val parts = line.split(" ")
+        (parts(0).toInt, parts(1).toInt)
+      }
+      case None => (0, 0)
+    }
+  }
+  def extractTondeusePosition(): List[(Int, Int, String)] = {
+    val lines = input.split("\n").filter(line => line.count(_.isDigit) >= 2)
+    lines.flatMap(line => {
+      val parts = line.split(" ")
+      if (parts.length == 3) Some((parts(0).toInt, parts(1).toInt, parts(2)))
+      else None
+    }).toList
+  }
+  def extractCommands(): List[String] = {
+    val lines = input.split("\n").filter(line => line.count(_.isDigit) == 0)
+    lines.toList
+  }
+}
